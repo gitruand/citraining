1ea3d65c54b3b20729c9a5b1d009c643205cbbb1
Truand <truand42@gmail.com>
2021-12-29 07:46:17 -0500

    minor modification

--- src/main/java/com/example/demo/HellocdApplication.java
+++ src/main/java/com/example/demo/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello World! madafacaaaas";
 	}
 
 	public static void main(String[] args) {
